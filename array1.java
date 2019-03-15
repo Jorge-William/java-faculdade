7f0204cc6a8ec359e9af807601fcaa145a29c8aa
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2019-03-15 18:06:30 -0300

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,29 @@
+package array;
+
+public class array1 {
+    public static void main(String args []){
+
+        String nome[] = new String[10];
+        int matricula[] = new int[5];
+        float nota[] = new float[15];
+
+        // Como se preenche um indice de um array
+
+        nome[0] = "Gabriel";
+        nome[1] = "Maria";
+
+        matricula[0] = 4411;
+        matricula[1] = 4400;
+
+        nota[0] = 5.6f;
+        nota[1] = 7.9f;
+
+
+        // outra forma de declarar array
+
+        String nomes[] = {"jorge", "William", "Cardoso"};
+        int matriculas[] = {2211, 2222, 2244};
+        float notas [] = {6.7f, 4.5f, 6.7f};
+
+    }
+}
