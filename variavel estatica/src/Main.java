0d5480f717292c33622f25de14af5ec11becee73
Jorge William <jorge_william_cardoso@yahoo.com.br>
2022-04-26 14:33:04 -0300

    Fix: Reordenando prints

--- variavel estatica/src/Main.java
+++ variavel estatica/src/Main.java
@@ -4,15 +4,14 @@ public class Main {
     public static void main(String[] args){
         Pessoa joao = new Pessoa();
         joao.name = "João";
+        System.out.println(joao.name + " - " + Pessoa.contador);
 
         Pessoa maria = new Pessoa();
         maria.name = "Maria";
+        System.out.println(maria.name + " - " + Pessoa.contador);
 
         Pessoa jose = new Pessoa();
         jose.name = "José";
-
-        System.out.println(joao.name + " - " + Pessoa.contador);
-        System.out.println(maria.name + " - " + Pessoa.contador);
         System.out.println(jose.name + " - " + Pessoa.contador);
 
 
